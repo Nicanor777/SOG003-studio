b1111ef63f459e0371e5a00a73b5b66811540e1f
Derrick Nicanor <dkahora@safaricom.co.ke>
2022-12-05 21:18:57 +0300

    code clean up on main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-// $(document).ready(function () {
-//   $("img.design1").hover(function(){
-//     $(this).css("text-align", "left");
-//     $(this).css("color", "pink")
-//     });
-// }); 
-
-// $(document).ready(function () {
-//   $("#design-image").hover(function () {
-//       $(this).stop().animate({
-//           opacity: .4
-//       }, 200);
-//       $("#design-image").slideDown('1500').hide('1000');
-//       $("#design").show('1500');
-//       // $("design").removeClass('hide');
-//   }, function () {
-//       $(this).stop().animate({
-//           opacity: 1
-//       }, 500);
-//       $("#design-image").slideUp('hide');
-//       $("#design").slideDown('1500').hide('1000');
-//   });
-// });
-
-
-
 $(document).ready(function() {
   $('#design-image').hover(function(){
   $("#design-image").slideDown('1500').hide('1000');
@@ -53,8 +27,6 @@ $(document).ready(function() {
   });
 });
 
-
-
 $(document).ready(function(){
   $("form#contact-form").submit(function(event){
     var name = $("input#name").val();
@@ -66,36 +38,63 @@ $(document).ready(function(){
     else {
       alert("Please enter your name and email!");
     }
-    
+  });
+});
+
+$(document).ready(function(){
+  $("#project1").mouseover(function(){
+    $("#work1").show();
+  }).mouseout(function(){
+    $("#work1").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#project2").mouseover(function(){
+    $("#work2").show();
+  }).mouseout(function(){
+    $("#work2").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#project3").mouseover(function(){
+    $("#work3").show();
+  }).mouseout(function(){
+    $("#work3").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#project4").mouseover(function(){
+    $("#work4").show();
+  }).mouseout(function(){
+    $("#work4").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#project5").mouseover(function(){
+    $("#work5").show();
+  }).mouseout(function(){
+    $("#work5").hide();
   });
 
-});  
+  $("#project6").mouseover(function(){
+    $("#work6").show();
+  }).mouseout(function(){
+    $("#work6").hide();
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready(function () {
-//   $("contact-form").submit(function (event) {
-//     // event.preventDefault();
-//     var name = $("input#name").val();
-//     var email = $("input#email").val();
-//     var message = $("textarea#contact").val();
-//     if ($("input#name").val() && $("input#email").val()) {
-//       alert(name + ", we have received your message. Thank you for reaching out to us.");
-//     }
-//     else {
-//       alert("Please enter your name and email!");
-//     }
-
-//   });
-// });
+  $("#project7").mouseover(function(){
+    $("#work7").show();
+  }).mouseout(function(){
+    $("#work7").hide();
+  });
+  
+  $("#project8").mouseover(function(){
+    $("#work8").show();
+  }).mouseout(function(){
+    $("#work8").hide();
+  });
+});
